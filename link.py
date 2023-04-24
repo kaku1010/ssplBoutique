21a0540914720b3a212f4aa86daadfa621134fb9
kaku1010 <Rita1041224@gmail.com>
2023-04-24 17:44:04 +0800

    2

--- link.py
+++ link.py
@@ -1,4 +1,9 @@
-import getpass
+import cx_Oracle
+cx_Oracle.init_oracle_client(lib_dir="./instantclient_19_8") # init Oracle instant client 位置
+connection = cx_Oracle.connect('GROUP3', '9bMnh4jMl0', cx_Oracle.makedsn('140.117.69.60', 1521, service_name='ORCLPDB1'))
+cursor = connection.cursor()
+
+'''import getpass
 import oracledb
 
 connection = oracledb.connect(
@@ -7,4 +12,4 @@ connection = oracledb.connect(
     dsn="140.117.69.60/ORCLPDB1")
 
 print("Successfully connected to Oracle Database")
-cursor = connection.cursor()
+cursor = connection.cursor()'''
